63fcb1d2bd725d284957c0a9faa5760614a72641
danilopgon <dani.lopezgonzalez.94@gmail.com>
2025-04-19 19:03:46 +0200

    Front/Back: Repo compartido para ambos

---
+++ frontend/src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import { routes } from './app.routes';
+import { primeNGThemeSettings } from './config/prime-ng-theme-settings';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG(primeNGThemeSettings),
+  ],
+};
