e0a91bca91a7c26b831cbfc905064d70a2f962ed
danilopgon <dani.lopezgonzalez.94@gmail.com>
2025-04-19 21:04:00 +0200

    Configurar guard para api key

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CitiesModule } from './modules/cities/cities.module';
 import { WeatherModule } from './modules/weather/weather.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [ConfigModule.forRoot(), WeatherModule, CitiesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
+    WeatherModule,
+    CitiesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ApiKeyGuard } from './core/guards/api-key.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  app.useGlobalGuards(new ApiKeyGuard(configService));
+
   await app.listen(3000);
 }
 bootstrap();
