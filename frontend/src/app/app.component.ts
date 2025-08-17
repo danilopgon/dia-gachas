d9b7ac3c83637771784880ee5be62adfbdbd4621
danilopgon <dani.lopezgonzalez.94@gmail.com>
2025-08-17 12:08:40 +0200

    FRONT: Añadir botón de información con popover en el componente principal y ajustar estilos en `result`.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { PopoverModule } from 'primeng/popover';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, PopoverModule],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
